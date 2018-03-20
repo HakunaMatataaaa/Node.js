f97c4975c60cc80d0b4f33c6774559ef922d8516
Diana Ayoub <diana..a.ayoob@gmail.com>
2018-03-20 00:03:19 +0000

    jgytftyf

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -1,8 +1,52 @@
-
 'use strict';
 
 var mongoose = require('mongoose'),
   Task = mongoose.model('Tasks');
+
+
+/// Get a single to-do with ID :id
+exports.readTodo = function (req, res) {
+  Task.findById(req.params.taskId, function (err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+/// Delete a task by id
+
+exports.clearTodos = function (req, res) {
+  Task.remove({
+    _id: req.params.taskId
+  }, function (err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task Successfully deleted' });
+  });
+};
+
+
+///  Sets the done flag of a single to-do to true, update true
+exports.markAsDone = function (req, res) {
+  Task.findOneAndUpdate({ _id: req.params.taskId }, req.body, { new: true }, function (err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+///  Sets the done flag of a single to-do to false, update false
+exports.markAsNotDone = function (req, res) {
+  Task.findOneAndUpdate({ _id: req.params.taskId }, req.body, { new: false }, function (err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+
 
 exports.list_all_tasks = function (req, res) {
   Task.find({}, function (err, task) {
@@ -13,42 +57,11 @@ exports.list_all_tasks = function (req, res) {
 };
 
 
-
-
 exports.create_a_task = function (req, res) {
   var new_task = new Task(req.body);
   new_task.save(function (err, task) {
     if (err)
       res.send(err);
     res.json(task);
-  });
-};
-
-
-exports.read_a_task = function (req, res) {
-  Task.findById(req.params.taskId, function (err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-
-exports.update_a_task = function (req, res) {
-  Task.findOneAndUpdate({ _id: req.params.taskId }, req.body, { new: true }, function (err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-
-exports.delete_a_task = function (req, res) {
-  Task.remove({
-    _id: req.params.taskId
-  }, function (err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Task successfully deleted' });
   });
 };
